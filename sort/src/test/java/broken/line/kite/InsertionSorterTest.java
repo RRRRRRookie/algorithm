766804ce5ed3e932ed0bc5f7871d6a74530dd352
RRRRRRookie <970584523@qq.com>
2023-05-01 09:39:39 +0800

    🖲 add log

---
+++ sort/src/test/java/broken/line/kite/InsertionSorterTest.java
@@ -0,0 +1,16 @@
+package broken.line.kite;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author: wanjia1
+ * @date: 2023/5/1
+ */
+class InsertionSorterTest {
+
+    @Test
+    void sort() {
+
+
+    }
+}
