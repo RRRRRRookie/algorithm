097e91b6547375a7c2576021232eba7f2c29b8e8
RRRRRRookie <970584523@qq.com>
2023-05-04 19:38:20 +0800

    🐢 binary tree 的先序 中序 后序遍历

--- data-algo/src/main/java/broken/line/kite/tree/BinaryTree.java
+++ data-algo/src/main/java/broken/line/kite/tree/BinaryTree.java
@@ -14,31 +14,49 @@ public class BinaryTree {
     // 递归序
     // TODO 构建二叉树
 
-    private void processFirst(Node node) {
+    private static void processFirst(Node node) {
         if (node == null) {
             return;
         }
-        System.out.println(node.value);
+        System.out.print(node.value);
         processFirst(node.left);
         processFirst(node.right);
     }
 
-    private void processMid(Node node) {
+    private static void processMid(Node node) {
         if (node == null) {
             return;
         }
         processMid(node.left);
-        System.out.println(node.value);
+        System.out.print(node.value);
         processMid(node.right);
     }
 
-    private void processBehind(Node node) {
+    private static void processBehind(Node node) {
         if (node == null) {
             return;
         }
         processBehind(node.left);
         processBehind(node.right);
-        System.out.println(node.value);
+        System.out.print(node.value);
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.left = new Node(2);
+        head.right = new Node(3);
+        head.left.left = new Node(4);
+        head.left.right = new Node(5);
+        head.right.left = new Node(6);
+        head.right.right = new Node(7);
+
+        processFirst(head);
+        System.out.println("========");
+        processMid(head);
+        System.out.println("========");
+        processBehind(head);
+        System.out.println("========");
+
     }
 
     public static class Node<T> {
