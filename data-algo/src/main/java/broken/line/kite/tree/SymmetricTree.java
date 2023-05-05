6a2638aa3a08afea88419bab5bdc20781c161f28
RRRRRRookie <970584523@qq.com>
2023-05-05 16:20:36 +0800

    🐢 binary tree isSymmetric

--- data-algo/src/main/java/broken/line/kite/tree/SymmetricTree.java
+++ data-algo/src/main/java/broken/line/kite/tree/SymmetricTree.java
@@ -5,6 +5,41 @@ package broken.line.kite.tree;
  * @date: 2023/5/5
  * 判断一棵树是否是镜面树
  * Leetcode原题，https://leetcode.com/problems/symmetric-tree
+ * 对于树中的任意两个对称节点L、R，必然有：
+ * <p>
+ * L 和 R 节点的值相等；
+ * L 的左节点和 R 的右节点对称
+ * L 的右节点和 R 的左节点对称
+ * 所以知道对称二叉树的定义后，可以看出来对称二叉树的定义是递归的，所以考虑从顶部节点开始往下递归，判断递归的每队节点是否对称，只要有一对节点不对称，那么整颗树就不是对称，反之，如果每对节点都对称，那么整棵树是对称。
  */
 public class SymmetricTree {
+
+    public static boolean isSymmetric(TreeNode node) {
+        return isMirror(node, node);
+    }
+
+    /**
+     * base case
+     * recursive formula
+     *
+     * @param node
+     * @param node1
+     * @return
+     */
+    private static boolean isMirror(TreeNode node, TreeNode node1) {
+        if (node == null ^ node1 == null) {
+            return false;
+        }
+        if (node == null && node1 == null) {
+            return true;
+        }
+        return node.val == node1.val && isMirror(node.left, node1.right) && isMirror(node.right, node1.left);
+    }
+
+    public static class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+    }
+
 }
