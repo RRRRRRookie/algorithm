9eae7a01d4cefa01d7ffb7aa153f11a03aedcddc
RRRRRRookie <970584523@qq.com>
2023-05-06 21:08:13 +0800

    🐢 binary tree path sum

--- data-algo/src/main/java/broken/line/kite/tree/PathSumTree.java
+++ data-algo/src/main/java/broken/line/kite/tree/PathSumTree.java
@@ -7,4 +7,39 @@ package broken.line.kite.tree;
  * Leetcode原题，https://leetcode.com/problems/path-sum
  */
 public class PathSumTree {
+
+    public static class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    class Solution {
+
+
+        public boolean hasPathSum(TreeNode root, int targetSum) {
+            if (root == null) {
+                return false;
+            }
+            return process(root, targetSum);
+        }
+
+        private boolean process(TreeNode root, int targetSum) {
+            if (root.left == null && root.right == null) {
+                return root.val == targetSum;
+            }
+            int rest = targetSum - root.val;
+            boolean leftAns = root.left != null && process(root.left, rest);
+            boolean rightAns = root.right != null && process(root.right, rest);
+            return leftAns | rightAns;
+        }
+
+
+    }
+
+
 }
