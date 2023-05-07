ec7ac99de74a8ddaecb82baccac98e3fbb6da69c
RRRRRRookie <970584523@qq.com>
2023-05-07 16:10:13 +0800

    🐢 merge sort

---
+++ sort/src/main/java/broken/line/kite/MergeSorter.java
@@ -0,0 +1,50 @@
+package broken.line.kite;
+
+/**
+ * @author: wanjia1
+ * @date: 2023/5/7
+ */
+public class MergeSorter {
+
+
+    public static void sort(int[] arr) {
+        if (arr == null || arr.length == 1) {
+            return;
+        }
+
+        process(arr, 0, arr.length - 1);
+
+    }
+
+    private static void process(int[] arr, int l, int r) {
+        if (l == r) {
+            return;
+        }
+        int mid = l + ((r - l) >> 1);
+        process(arr, l, mid);
+        process(arr, mid + 1, r);
+        merge(arr, l, mid, l);
+    }
+
+    private static void merge(int[] arr, int l, int mid, int r) {
+        int[] help = new int[r - l + 1];
+        int i = 0;
+        int p1 = l;
+        int p2 = mid + 1;
+        while (p1 <= mid && p2 <= r) {
+            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= mid) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= r) {
+            help[i++] = arr[p2++];
+        }
+        for (int j = 0; j < help.length; j++) {
+            arr[l + j] = help[j];
+        }
+
+    }
+
+
+}
