ab80ca8db39568246bdf371010c5a69158bc6a60
RRRRRRookie <970584523@qq.com>
2023-10-03 13:59:48 +0800

    🎉 begin again

---
+++ practice/src/main/java/broken/line/kite/TestDemo.java
@@ -0,0 +1,17 @@
+package broken.line.kite;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author: wanjia1
+ * @date: 2023/10/3
+ */
+@Slf4j
+public class TestDemo {
+
+
+    public static void main(String[] args) {
+        log.info("initial proj");
+    }
+
+}
